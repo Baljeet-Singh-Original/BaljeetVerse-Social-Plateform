7eea38ef6c7759550580a1513a4a045af254db10
Baljeet Singh <baljeetsinghsomvanshi8745@gmail.com>
2021-04-26 22:51:59 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
+
 const app = express();
 connectDB();
 
@@ -14,6 +16,19 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 app.use('/api/profile', require('./routes/api/profile'));
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+
 
 const PORT = process.env.PORT || 4000;
 
