9da8a69df432eb581b3422bbaff5839ab0a7c6dd
Baljeet Singh <baljeetsinghsomvanshi8745@gmail.com>
2021-05-06 17:30:27 +0530

    heroku deployment

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ app.use(express.json({ extended: false }));
 // app.get('/', (req, res) => res.send('API running.'));
 
 app.use('/api/users', require('./routes/api/user'));
-
 app.use('/api/posts', require('./routes/api/posts'));
-
 app.use('/api/auth', require('./routes/api/auth'));
-
 app.use('/api/profile', require('./routes/api/profile'));
 
 
